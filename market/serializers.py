24f9c6d04cd7c8639fbf1df43fba0044e3c2d5a0
Kumar-Sai99 <Kumar.pajjuru@wmlit.com>
2026-05-29 11:47:37 +0530

    update message

--- market/serializers.py
+++ market/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Category, Crop, FarmerProfile, BuyerProfile
+from .models import Category, Crop, FarmerProfile
 
 
 # ── CATEGORY SERIALIZER ───────────────────────────────
